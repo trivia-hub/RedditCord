732e371dd370ce39a10ff4154b7698959b514a5a
vilP1L <vilP1l@github.com>
2019-06-24 16:07:25 -0400

    Add ping command

---
+++ src/commands/ping.js
@@ -0,0 +1,16 @@
+import { MessageEmbed } from 'discord.js';
+
+export const run = async (client, msg) => {
+  const embed = new MessageEmbed()
+    .setTitle(':ping_pong:')
+    .addField(`**Ping**: ${Math.round(client.ws.ping)}ms`, '[]()')
+    .addField('**Latency**: Loading...', '[]()');
+  const m = await msg.channel.send(embed);
+  const start = Date.now();
+  embed.fields[1].name = '**Latency**: Loaded!';
+  await m.edit(embed);
+  embed.fields[1].name = `**Latency**: ${Date.now() - start}ms`;
+  m.edit(embed);
+};
+
+export const name = 'ping';
