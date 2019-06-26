8fbdfb526b305b20b01ee19ad91357789b8e9849
vilP1L <vilP1l@github.com>
2019-06-25 22:07:10 -0400

    Add base reddit class

---
+++ src/Classes/Reddit.js
@@ -0,0 +1,49 @@
+import { EventEmitter } from 'events';
+import request from 'request';
+import { promisify } from 'util';
+import { webserver, reddit } from '../../config';
+
+export default class Reddit extends EventEmitter {
+  constructor(refreshToken, oauth) {
+    super();
+    this.refreshToken = refreshToken;
+    this.oauth = oauth;
+    this.accessToken = null;
+    this.req = promisify(request);
+  }
+
+  async getAccessToken() {
+    const { body } = await this.req('https://www.reddit.com/api/v1/access_token', {
+      method: 'POST',
+      body: `grant_type=authorization_code&code=${this.oauth}&redirect_uri=http://${webserver.host}:${webserver.port}/${webserver.endpoints.login}`,
+      headers: {
+        Authorization: reddit.auth,
+      },
+    });
+    return JSON.parse(body);
+  }
+
+  async refreshAccessToken() {
+    const { body } = await this.req('https://www.reddit.com/api/v1/access_token', {
+      method: 'POST',
+      body: `grant_type=refresh_token&refresh_token=${this.refreshToken}`,
+      headers: {
+        Authorization: reddit.auth,
+      },
+    });
+    this.accessToken = JSON.parse(body).access_token;
+    return this.accessToken;
+  }
+
+  async getSubredditPosts(subreddit, filter, before, after) {
+    let url = `https://reddit.com/r/${subreddit}/${filter}.json?limit=100`;
+    if (before) url += `&before=${before}`;
+    if (after) url += `&after=${after}`;
+    const { body } = await this.req(url, { json: true });
+    return {
+      posts: body.data.children.map(c => c.data),
+      before: body.data.before,
+      after: body.data.after,
+    };
+  }
+}
