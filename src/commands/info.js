da470d619cc46d9789b8763bddb19ebc7cb4bbea
vilP1L <vilP1l@github.com>
2019-06-26 21:49:29 -0400

    info embed fixes

--- src/commands/info.js
+++ src/commands/info.js
@@ -3,8 +3,8 @@ import { MessageEmbed } from 'discord.js';
 export const run = (client, msg) => {
   const embed = new MessageEmbed()
     .setTitle('Info')
-    .setDescription('RedditCord is an opensource Discord Reddit Client created by vilP1l#0001, SpotiKona#0001 and its a-me markio#9733 created for Discord Hack Week.')
-    .addField('Github', 'https://github.com/trivia-hub/RedditCord')
+    .setDescription('RedditCord is an open source Discord Reddit Client created by vilP1l#0001, SpotiKona#0001, and its a-me markio#9733 for Discord Hack Week.')
+    .addField('GitHub', 'https://github.com/trivia-hub/RedditCord')
     .setFooter('For command help run +help');
   msg.channel.send(embed);
 };
