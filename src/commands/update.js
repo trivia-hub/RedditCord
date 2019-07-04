247881840125881864f83bc74363f877bc660252
vilP1L <vilP1l@github.com>
2019-07-04 16:37:35 -0400

    fix reload command

--- src/commands/update.js
+++ src/commands/update.js
@@ -3,11 +3,14 @@ import { get } from 'node-cmd';
 
 export const run = async (client, msg) => {
   const runCmd = promisify(get);
-  const m = await msg.channel.send('(1/3) Pulling changes...');
+  const m = await msg.channel.send('(1/4) Pulling changes...');
   await runCmd('git pull');
-  await m.edit('(2/3) Building changes...');
+  await m.edit('(2/4) Updating dependencies...')
+  await runCmd('npm install');
+  await m.edit('(3/4) Building changes...');
   await runCmd('npm run build');
-  await m.edit('(3/3) Reloading commands...');
+  await m.edit('(4/4) Reloading commands...');
+  client.reloadCommands();
   m.edit('Finished updating!');
 };
 
