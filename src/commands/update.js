a2c6c5c4f2664033f8500a2b2d8f48652f9534cd
vilP1L <vilP1l@github.com>
2019-07-05 19:00:04 -0400

    Fix linting error

--- src/commands/update.js
+++ src/commands/update.js
@@ -5,7 +5,7 @@ export const run = async (client, msg) => {
   const runCmd = promisify(get);
   const m = await msg.channel.send('(1/4) Pulling changes...');
   await runCmd('git pull');
-  await m.edit('(2/4) Updating dependencies...')
+  await m.edit('(2/4) Updating dependencies...');
   await runCmd('npm install');
   await m.edit('(3/4) Building changes...');
   await runCmd('npm run build');
